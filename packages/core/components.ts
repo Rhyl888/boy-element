747a5b58146e6c2ac6a1695192620e81878b1211
鹿森 <2231752346@qq.com>
2024-06-02 14:04:07 +0800

    feat: add Collapse基础

--- packages/core/components.ts
+++ packages/core/components.ts
@@ -1,4 +1,4 @@
-import { ErButton, ErIcon } from '../components';
+import { ErButton, ErIcon, ErCollapse } from '../components';
 import type { Plugin } from 'vue';
 
-export default [ErButton, ErIcon] as Plugin[];
+export default [ErButton, ErIcon, ErCollapse] as Plugin[];
